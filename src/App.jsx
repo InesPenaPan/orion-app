bca6d8a0af87bbad15fdeb267c0c8ca34b1fd269
Inés Peña <ipenapando@gmail.com>
2026-01-22 14:45:54 +0100

    List clients funciona

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
 import ClientPortfolioPage from './pages/ClientPortfolioPage';
 import OpportunityListPage from './pages/OpportunityListPage';
+import ClientListPage from './pages/ClientListPage';
 import ClientDetailPage from './pages/ClientDetailPage';
 import './app.css'
 
@@ -12,6 +13,7 @@ const App = () => (
       <Route path="/" element={<LoginPage />} />
       <Route path="/home" element={<HomePage />} />
       <Route path="/portfolio" element={<ClientPortfolioPage />} />
+      <Route path="/clients" element={<ClientListPage/>} />
       <Route path="/pipeline" element={<OpportunityListPage/>} />
       <Route path="/client/:clientName" element={<ClientDetailPage />} />
     </Routes>
